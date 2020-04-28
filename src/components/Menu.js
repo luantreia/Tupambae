f5d4d1b0357a331f18ff4d97573a4c4d9b78a3ff
luan <luantreia@gmail.com>
2020-04-28 18:25:52 -0300

    28/4

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,27 +3,32 @@ import './Menu.css';
 import './Page.js';
 
 function Menu() {
+    const [Page] = useState('map');
+
     return (
         <div className="w3-display-bottommiddle">
             <div className="menu-icons w3-card w3-margin">
                 <button 
                     className="material-icons w3-padding" 
+                    onClick= {() => Page('map')}
                 >
                     map
                 </button>
                 <button 
                     className="material-icons w3-padding"
+                    onClick= {() => Page('account')}
                 >
                     account_circle
                 </button>
                 <button 
                     className="material-icons w3-padding"
+                    onClick= {() => Page('search')}
                 >
                     search
                 </button>
+                
             </div>
         </div>
     );
 }
-
 export default Menu;
