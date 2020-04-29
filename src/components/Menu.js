68632594bc8903885a2cd781d567a6d3fd14ad81
luan <luantreia@gmail.com>
2020-04-28 22:41:36 -0300

    28/4

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,28 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Menu.css';
 import './Page.js';
 
 function Menu() {
-    const [Page] = useState('map');
+    const [Page, setPage] = useState('Map');
+
+    useEffect(() => {
+        return ('./pages/{Page}');
+    });
 
     return (
         <div className="w3-display-bottommiddle">
             <div className="menu-icons w3-card w3-margin">
                 <button 
                     className="material-icons w3-padding" 
-                    onClick= {() => Page('map')}
+                    onClick= {() => setPage('Map')}
                 >
                     map
                 </button>
                 <button 
                     className="material-icons w3-padding"
-                    onClick= {() => Page('account')}
+                    onClick= {() => setPage('Account')}
                 >
                     account_circle
                 </button>
                 <button 
                     className="material-icons w3-padding"
-                    onClick= {() => Page('search')}
+                    onClick= {() => setPage('Search')}
                 >
                     search
                 </button>
